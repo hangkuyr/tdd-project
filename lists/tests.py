b4ef2edfa1b4593776c2a0071f548c76a583f75a
hangkuyr <laisglima@hotmail.com>
2022-05-19 17:37:22 -0300

    Novo Projeto: Atualizando o Template

--- lists/tests.py
+++ lists/tests.py
@@ -63,20 +63,11 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{my_list.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_only_items_for_that_list(self):
-        correct_list = List.objects.create()
-        Item.objects.create(text='itemey 1', list=correct_list)
-        Item.objects.create(text='itemey 2', list=correct_list)
+    def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
-        Item.objects.create(text='other list item 1', list=other_list)
-        Item.objects.create(text='other list item 2', list=other_list)
-
+        correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
-
-        self.assertContains(response, 'itemey 1')
-        self.assertContains(response, 'itemey 2')
-        self.assertNotContains(response, 'other list item 1')
-        self.assertNotContains(response, 'other list item 2')
+        self.assertEqual(response.context['list'], correct_list)
 
 class NewItemTest(TestCase):
 
